4266510606ca8efc593fca9347c8f2e41f35261b
cxb <cxb@gmail.com>
2022-11-27 23:14:57 +0800

    初始化工程

---
+++ src/main/java/com/cxb/storehelperserver/StorehelperserverApplication.java
@@ -0,0 +1,13 @@
+package com.cxb.storehelperserver;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StorehelperserverApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(StorehelperserverApplication.class, args);
+	}
+
+}
